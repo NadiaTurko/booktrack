6fe1fcd49e4ec0cadea942be37bf939fba31ba56
NadiaTurko <47818620+NadiaTurko@users.noreply.github.com>
2025-11-03 11:58:56 +0200

    Delete component TitleBooksPage'

--- src/containers/page/BooksPage.jsx
+++ src/containers/page/BooksPage.jsx
@@ -1,7 +1,6 @@
 import { useBooks } from "../../context/BooksContext";
 import ShowMoreButton from "../../components/Buttons/ShowMoreButton";
 import BooksList from "../../components/BooksList/BooksList";
-import TitleBooksPage from "../../components/Title/TitleBooksPage";
 
 const BooksPage = () => {
   const { books, totalBooks, status, loadMoreBooks } = useBooks();
@@ -10,7 +9,6 @@ const BooksPage = () => {
 
   return (
     <div className="flex flex-col items-center space-y-6">
-      <TitleBooksPage text="Hello BookTrack" />
       <BooksList items={books} status={status} />
       <ShowMoreButton onClick={loadMoreBooks} disabled={showMoreDisabled} />
     </div>
